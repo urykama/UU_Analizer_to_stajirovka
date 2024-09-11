023e62093d2964e4fcba92816e2f394e9fcdac08
URAL KAMALETDINOV <urykama@gmail.com>
2024-09-11 23:22:32 +0500

    технические правки

--- project.py
+++ project.py
@@ -6,14 +6,12 @@ import pandas as pd
 class PriceMachine:
 
     def __init__(self):
-        self.data = []
+        # self.data = []
         # self.result = ''
         # self.name_length = 0
-        self.prices = {}
+        self.prices = pd.DataFrame(columns=['№', 'Наименование', 'Цена', 'Вес', 'Цена за кг.', 'Файл'])
 
     def load_prices(self, file_path=os.path.dirname(os.path.realpath(__file__))):
-        df2 = pd.DataFrame(columns=['№', 'Наименование', 'Цена', 'Вес', 'Цена за кг.', 'Файл'])
-        df2.dropna(axis=1, how='all', inplace=True)
         for file in os.listdir(file_path):
             if file.endswith('.csv') and 'price' in file:
                 print(f'чтение данных из файла {file}')
@@ -33,19 +31,14 @@ class PriceMachine:
                 new_df['Вес'] = new_df['Вес'].astype(float)
                 new_df['Цена за кг.'] = round(new_df['Цена'] / new_df['Вес'], 2)
                 new_df.dropna(axis=1, how='all', inplace=True)
-                df2 = pd.concat([df2, new_df])
-        df2.sort_values(by='Цена за кг.', ascending=True, inplace=True)
-        df2 = df2.reset_index(drop=True)
-        df2['№'] = range(1, len(df2) + 1)
-        df2 = df2.reindex(columns=['№', 'Наименование', 'Цена', 'Вес', 'Цена за кг.', 'Файл'])
-        self.prices['result'] = df2
-        return df2
+                self.prices.dropna(axis=1, how='all', inplace=True)
+                self.prices = pd.concat([self.prices, new_df])
+        self.prices.sort_values(by='Цена за кг.', ascending=True, inplace=True)
+        self.prices = self.prices.reset_index(drop=True)
+        self.prices['№'] = range(1, len(self.prices) + 1)
+        return self.prices
 
     def _search_product_price_weight(self, headers):
-        # col_product = None
-        # col_price = None
-        # col_weight = None
-
         for i, header in enumerate(headers):
             if re.match(r'^название|^товар|^наименование|^продукт$', headers[header][0], re.IGNORECASE):
                 col_product = headers[header]
@@ -53,7 +46,6 @@ class PriceMachine:
                 col_price = headers[header]
             elif re.match(r'^вес|^масса|^фасовка$', headers[header][0], re.IGNORECASE):
                 col_weight = headers[header]
-
         return col_product, col_price, col_weight
 
     def export_to_html(self, fname='output.html'):
@@ -74,31 +66,27 @@ class PriceMachine:
                     <th>Цена за кг.</th>
                 </tr>
         '''
-        for file in self.prices:
-            df = self.prices['result']
-            for i in range(len(df)):
-                result += '<tr>'
-                result += f'<td>{df["№"].values[i]}</td>'
-                result += f'<td>{df["Наименование"].values[i]}</td>'
-                result += f'<td>{df["Цена"].values[i]}</td>'
-                result += f'<td>{df["Вес"].values[i]}</td>'
-                result += f'<td>{df["Файл"].values[i]}</td>'
-                result += f'<td>{df["Цена за кг."].values[i]}</td>'
-                result += '</tr>'
-            result += '</table>'
+        for i in range(len(self.prices)):
+            result += '<tr>'
+            result += f'<td>{self.prices["№"].values[i]}</td>'
+            result += f'<td>{self.prices["Наименование"].values[i]}</td>'
+            result += f'<td>{self.prices["Цена"].values[i]}</td>'
+            result += f'<td>{self.prices["Вес"].values[i]}</td>'
+            result += f'<td>{self.prices["Файл"].values[i]}</td>'
+            result += f'<td>{self.prices["Цена за кг."].values[i]}</td>'
+            result += '</tr>'
+        result += '</table>'
         with open(fname, 'w') as f:
             f.write(result)
 
     def find_text(self, search_text):
-        for file in self.prices:
-            df = self.prices[file]
-            matches = df[df['Наименование'].str.contains(search_text, case=False)]
-            if len(matches) > 0:
-                return matches
-            else:
-                print(f'Продукта с наименованием: {search_text}, не найдено. '
-                      f'Для примера выводим данные по запросу: "Пелядь крупная х/к потр"')
-                return pm.find_text("Пелядь крупная х/к потр")
+        matches = self.prices[self.prices['Наименование'].str.contains(search_text, case=False)]
+        if len(matches):
+            return matches
+        else:
+            print(f'Продукта с наименованием: {search_text}, не найдено. '
+                  f'Для примера выводим данные по запросу: "Пелядь крупная х/к потр"')
+            return self.find_text('Пелядь крупная х/к потр')
 
 
 if __name__ == "__main__":
